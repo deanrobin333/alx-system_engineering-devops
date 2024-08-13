6461d063f1e4577e1ae66c11b3aa0da8e11455d3
deanrobin333 <deanrobin777@gmail.com>
2024-08-14 01:02:17 +0300

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 1-top_ten.py
+""" Script to get the first 10 hot
+    posts on Reddit
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """get first 10 hot post for a subreddit"""
+    if subreddit and type(subreddit) is str:
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        headers = {'user-agent': 'my-app/0.0.1'}
+        params = {'limit': 10}
+        req = get(url, params=params, headers=headers, allow_redirects=False)
+        if req.status_code == 200:
+            data = req.json()
+            posts = data.get('data', {}).get('children', {})
+            for post in posts:
+                print(post.get('data').get('title'))
+        else:
+            print(None)
