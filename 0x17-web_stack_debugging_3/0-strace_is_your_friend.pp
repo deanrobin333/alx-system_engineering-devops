8c58915722567271defe1dfbca837cc0d7838ecf
deanrobin333 <deanrobin777@gmail.com>
2024-08-15 01:27:13 +0300

    0-strace_is_your_friend.pp && readme

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# 0-strace_is_your_friend.pp
+# Fixes a faulty wordpress site
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
